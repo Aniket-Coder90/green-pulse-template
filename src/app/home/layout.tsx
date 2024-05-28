86800256b0bca0132bd2a8778bdcbfd11df38beb
Aniket-Coder90 <aniket18798@gmail.com>
2024-05-28 12:20:22 +0530

    feat: Project Folder Structure Initialize

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,18 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    default: 'Green Pulse Template',
+    default: 'Home | Green Pulse Template',
     template: '%s | Green Pulse Template'
   },
   description: "Main Layout"
 };
 
-export default function RootLayout({
+export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
